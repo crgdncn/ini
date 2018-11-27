0430d103a3d2c0c87c639ae4511a437cad0805d6
crgdncn <crgdncn@gmail.com>
2018-11-27 17:23:18 -0500

    code clean up

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,8 @@ const mix = require('laravel-mix');
 mix.options({ processCssUrls: false });
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js/app.js')
+   .styles('resources/css/app.css', 'public/css/app.css');
 
 // version files in production
 if (mix.inProduction()) {
