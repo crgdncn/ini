51e429d642a7e8978ce9601cfe1669fb433b1095
Craig Duncan <cduncan@filtereasy.com>
2018-12-12 15:32:45 -0500

    added files/sections/show view

--- resources/views/files/sections/show.blade.php
+++ resources/views/files/sections/show.blade.php
@@ -1,1 +1,45 @@
-show.blade.php
+@extends('layouts.app')
+
+@section('title', 'File Section')
+
+@section('content')
+     <p>
+        <a href="{{route('files.files.index')}}"> Files </a>
+        / <a href="{{route('files.files.show', $file)}}">{{$file->name}}</a>
+        / {{$section->name}}
+    </p>
+    <table class="table table-bordered table-striped">
+        <tbody>
+            <tr>
+                <th width="5%">ID</th>
+                <td>{{$section->id}}</td>
+            </tr>
+            <tr>
+                <th width="20%">Name</th>
+                <td>{{$section->name}}</td>
+            </tr>
+            <tr>
+                <th>Description</th>
+                <td>{{$section->description}}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <p>Keys</p>
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr class="text-left">
+                <th width="5%">ID</th>
+                <th width="20%">Name</th>
+                <th class="text-center">Description</th>
+                <th width="10%"></th>
+            </tr>
+        </thead>
+        <tbody id="tbody">
+        @foreach($section->keys as $key)
+            @include('files.file.keys.partials.keyTableRow')
+        @endforeach
+        </tbody>
+    </table>
+    <button class="btn btn-primary" onClick="getFormModal('{ {route('ini.types.sections.keys.create', [$type, $section])} }', 'New Key')">Add Keys</button>
+@endsection
---
+++ resources/views/files/sections/tableRow.blade.php
@@ -0,0 +1,22 @@
+<tr id="trow_{{$section->id}}">
+    <td>{{$section->id}}</td>
+    @php
+        // create href url and form to give values to each key
+    @endphp
+    <td id="name"><a href="{{route('files.file.sections.show', [$file, $section])}}">{{$section->name}}</a></td>
+
+    <td style="text-align:center">{{$section->keyCount}}</td>
+    <td>
+        <button
+            class="btn btn-sm"
+            type="button"
+            onClick="postObjectDelete('delete-type-{{$section->id}}', {{$section->id}})"
+        >
+        Delete Section
+        </button>
+    <form id="delete-type-{{$section->id}}" action="{{route('files.file.sections.destroy', [$file, $section])}}">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+    </form>
+    </td>
+</tr>
