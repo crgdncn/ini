5a75094e744f18d246eff7054e4b5ddd57a28df2
Craig Duncan <cduncan@filtereasy.com>
2019-01-04 14:28:08 -0500

    added custom error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Oops')
+
+@section('content')
+    <h1>Page Not Found</h1>
+    <p>We're Sorry. The page you are looking for does not exist.</p>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Oops')
+
+@section('content')
+    <h1>Oops</h1>
+    <p>We're Sorry. There was an error processing your request.</p>
+@endsection
